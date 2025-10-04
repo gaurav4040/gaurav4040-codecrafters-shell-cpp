6e6a92082211656430cb82cad26c81bff5a81bc6
gaurav4040 <jangragaurav593@gmail.com>
2025-10-04 23:30:26 +0530

    type

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,19 @@ int main() {
       }
       continue;
     }
+
+    if(word=="type"){
+      std::string word2;
+      std::string remainingType;
+      std::istringstream ist(remaining);
+      ist>>word2;
+      std::getline(ist,remainingType);
+      if((word2=="type"||word2=="echo"||word2=="exit")&&remainingType.empty()||remainingType.find_first_not_of(" \t\n\r")==std::string::npos){
+        std::cout<< word2 << " is a shell builtin"<<std::endl;
+        continue;
+      }
+    }
+
     std::cout << input << ": command not found" << std::endl;
   }
 
