405c462a972612f9a13712ee8750c317945d6655
gaurav4040 <jangragaurav593@gmail.com>
2025-10-04 22:52:41 +0530

    exit

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,9 @@ int main() {
     std::cout<<"$ ";
     std::string input;
     std::getline(std::cin, input);
+    if(input=="exit 0"){
+      return 0;
+    }
     std::cout << input << ": command not found" << std::endl;
   }
-
 }
